db1bbdcb4e159dcbd93195a2aa0f442230895f06
Asem <asem.mahmod@gmail.com>
2020-03-30 12:32:38 +0200

    Updates

---
+++ precache-manifest.585d3b97b79c7bc114f57d9381ddc921.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1009655a7fdb9c485f161954c736d0a7",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "427472972b28c62bd035",
+    "url": "/todo-list/static/css/main.d5156671.chunk.css"
+  },
+  {
+    "revision": "55e8f88b0d9096d005dd",
+    "url": "/todo-list/static/js/2.83619aba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.83619aba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "427472972b28c62bd035",
+    "url": "/todo-list/static/js/main.f6fea0d7.chunk.js"
+  },
+  {
+    "revision": "4c207b602d40809001a5",
+    "url": "/todo-list/static/js/runtime-main.2929926a.js"
+  }
+]);
